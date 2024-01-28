b0aa4fe17f8b8e52a1cc92dd07f3ededeaaa152a
agustin9700 <ryuk10xd@gmail.com>
2024-01-28 00:59:04 -0300

    agregando register y login

--- app.js
+++ app.js
@@ -11,6 +11,14 @@ app.get("/",function(req,res){
     const ruta= path.join(__dirname,("/views/home.html"));
     res.sendFile(ruta)
 })
+app.get("/registro",function(req,res){
+    const ruta= path.join(__dirname,("/views/register.html"));
+    res.sendFile(ruta)
+})
+app.get("/login",function(req,res){
+    const ruta= path.join(__dirname,("/views/login.html"));
+    res.sendFile(ruta)
+})
 
 app.listen(3030,()=> //app seria un objeto,dentro tiene metodos. al poner .listen llamamos a esa funcion
     console.log("server iniciado")
